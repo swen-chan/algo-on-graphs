1a52320882e7538203f2fb8e3c0cbfb1a48c11dd
SWENCHAN <112753032+swen-chan@users.noreply.github.com>
2022-12-17 12:58:31 +0800

    Update bipartite.py
    Your code will not work if the graph exists two or more connected components.I have did some revisions with

--- week 3/bipartite/bipartite.py
+++ week 3/bipartite/bipartite.py
@@ -1,5 +1,24 @@
-def bipartite(adj):
-    #write your code here
+#The first function is basically origin,but change the auguments.
+def bipartite(s,color_arr):
+    queue = [s]
+    while queue:
+        u = queue.pop(0)
+        for v in adj[u]:
+            if color_arr[v] == -1:
+                color_arr[v] = 1 - col[u]
+                queue.append(v)
+            elif color_arr[v] == color_arr[u]:
+                return False
+    return True
+#this function is to traveral every node,avoiding disconnected graph.
+def ispartite(adj):
+    color_arr = [-1] * len(adj)
+    for i in range(len(adj)):
+        if color_arr[i] == -1:
+            if not bipartite(i, color_arr):
+                return 0
+    return 1
+'''
     color_arr = [-1] * len(adj)
     color_arr[0] = 1
     queue = []
@@ -13,7 +32,7 @@ def bipartite(adj):
             elif color_arr[v] == color_arr[u]:
                 return 0
     return 1
-
+'''
 if __name__ == '__main__':
     n, m = map(int, raw_input().split())
     adj = [[] for _ in range(n)]
@@ -22,4 +41,6 @@ if __name__ == '__main__':
         # adjacency list		
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(bipartite(adj))
+    #change the function into print
+    print(ispartite(adj))
+    
